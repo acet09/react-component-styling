db798b3526dbfa6d6df6140413ac646b3baa77a7
acet09 <ysjungman@gmail.com>
2023-03-27 08:31:49 +0900

    84_동적_인라인스타일_설정하기(2)

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,11 +5,11 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
-  const [isValid, setIsvalid] = useState(true);
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
-    if (event.target.value.trim().langth > 0) {
-      setIsvalid(true);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
     }
     setEnteredValue(event.target.value);
   };
@@ -17,7 +17,7 @@ const CourseInput = props => {
   const formSubmitHandler = event => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
-      setIsvalid(false);
+      setIsValid(false);
       return;
     }
     props.onAddGoal(enteredValue);
@@ -25,12 +25,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
-        <input style={{
-          borderColor: !isValid ? 'red' : 'black',
-          background: !isValid ? 'salmon' : 'transparent'
-        }} type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
